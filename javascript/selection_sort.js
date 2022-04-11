14be63828f034ea75cfec7c062add9c36ccf7876
Olha Gruzglina <olga.zhmura@gmail.com>
2022-04-11 12:15:17 -0400

    commit tree 7466b875a76261d99206751d87db23d151926813

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,11 +1,40 @@
+//let sortedArr = []; - for recurssion
 function selectionSort(arr) {
+  let sortedArr = [];
   // type your code here
+  //let min = arr[0];
+  
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (min > arr[i]) {
+  //     min = arr[i]
+  //   }
+  // }
+  //or
+  // let min = Math.min(...arr);
+  //with recurssion 
+  // let index = arr.indexOf(min);
+  // arr.splice(index, 1);
+  // sortedArr.push(min);
+  
+  // if (arr.length !== 1)
+  //   selectionSort(arr);
+  // else
+  //   sortedArr.push(arr[0]);
+
+    //or
+    while(arr.length !== 0) {
+      let min = Math.min(...arr);
+      let index = arr.indexOf(min);
+      arr.splice(index, 1);
+      sortedArr.push(min);
+    } 
+return sortedArr;
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: [-1, 2, 3, 5]");
-  console.log("=>", selectionSort([3, -1, 5, 2]));
+  console.log("Expecting: [-1, 2, 3, 3, 5]");
+  console.log("=>", selectionSort([3, -1, 5,3, 2]));
 
   console.log("");
 
